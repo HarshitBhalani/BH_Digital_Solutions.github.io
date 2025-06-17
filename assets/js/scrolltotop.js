c5cdc5ca5bfde7cb2d7cda3b3ea2f196c394dea5
HarshitBhalani <harshitbhalani187@gmail.com>
2025-06-17 14:48:32 +0530

    updated

---
+++ assets/js/scrolltotop.js
@@ -0,0 +1,21 @@
+let calcScrollValue = () => {
+  let scrollProgress = document.getElementById("progress");
+  let progressValue = document.getElementById("progress-value");
+  let pos = document.documentElement.scrollTop;
+  let calcHeight =
+    document.documentElement.scrollHeight -
+    document.documentElement.clientHeight;
+  let scrollValue = Math.round((pos * 101) / calcHeight);
+  if (pos > 101) {
+    scrollProgress.style.display = "grid";
+  } else {
+    scrollProgress.style.display = "none";
+  }
+  scrollProgress.addEventListener("click", () => {
+    document.documentElement.scrollTop = 0;
+  });
+  scrollProgress.style.background = `conic-gradient(#FA6DB5 ${scrollValue}%, #ffffff ${scrollValue}%)`;
+};
+
+window.onscroll = calcScrollValue;
+window.onload = calcScrollValue;
